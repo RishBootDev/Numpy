a3943bba8da0be625e9e6a88e77ccf9262424a7c
Rishabh <rishabhdubey1472004@gmail.com>
2026-09-21 13:33:40 +0530

    updated

---
+++ built_in_functions.py
@@ -0,0 +1,6 @@
+a = np.array([1, 2, 3, 4, 5])
+
+print(np.sum(a))         # Output - 15
+print(np.prod(a))        # Output - 120
+print(np.cumsum(a))      # Output - [1 3 6 10 15]
+print(np.cumprod(a))     # Output - [1 2 6 24 120]
---
+++ scalar_operations.py
@@ -0,0 +1,7 @@
+# scalar operations
+
+a = np.array([1, 2, 3])
+result = a + 10       # Output - [11, 12, 13]
+result = a - 10       # Output - [-9, -8, -7]
+result = a * 10       # Output - [10, 20, 30]
+result = a / 10       # Output - [0.1, 0.2, 0.3]
