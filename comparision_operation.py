ff0c4d92eb605e696fbeabd0b08c35f4b3202f6f
Rishabh <rishabhdubey1472004@gmail.com>
2026-09-21 13:54:10 +0530

    updated

---
+++ comparision_operation.py
@@ -0,0 +1,8 @@
+array1 = np.array([1, 2, 3, 4, 5])
+array2 = np.array([5, 4, 3, 2, 1])
+
+print(array1 > array2)      # Output - [False False False  True  True]
+print(array1 < array2)      # Output - [ True  True False False False]
+print(array1 == array2)     # Output - [False False  True False False]
+
+# so basically the operation on arrays in python signifies the operation at elements present at the common index of both the array
