1f9e6770b0c2a8daa1253cc980f11367890266b3
Rishabh <rishabhdubey1472004@gmail.com>
2026-09-21 13:26:59 +0530

    updated

---
+++ element_wise.py
@@ -0,0 +1,8 @@
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print(a + b)      # Output - [5 7 9]
+print(a - b)      # Output - [-3 -3 -3]
+print(a * b)      # Output - [4 10 18]
+print(a / b)      # Output - [0.25 0.4 0.5]
+print(b % a)      # Output - [0 1 0]
